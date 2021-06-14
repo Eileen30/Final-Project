4f2b1d767591f89f73a8d2705db6e3c55b8de144
Eileen Ninan <47881456+Eileen30@users.noreply.github.com>
2021-06-14 19:05:42 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User,audiofiles,compareaudiofiles
-
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(audiofiles)
-admin.site.register(compareaudiofiles)
